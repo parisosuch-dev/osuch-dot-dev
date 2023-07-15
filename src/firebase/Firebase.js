750bb688e4c04122481846e80d20c09775d62db5
parisosuch-dev <parisosuch-dev@gmail.com>
2023-07-15 14:00:37 -0700

    added auth and db exports for firebase sdk

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,3 +29,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// firebase exports for API
+export const auth = getAuth(app);
+export const db = getFirestore(app);
