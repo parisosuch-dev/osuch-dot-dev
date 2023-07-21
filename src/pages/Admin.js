3d5fb58d810d36de718f81d9cb658e489071d7c2
parisosuch-dev <parisosuch-dev@gmail.com>
2023-07-20 20:36:43 -0700

    added auth functionality to admin panel

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // firebase
 import { storage } from "../firebase/Firebase";
 import { postBlog } from "../firebase/BlogHandler";
 import { uploadBytes, ref } from "firebase/storage";
+import { auth } from "../firebase/Firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export const Admin = () => {
+    // view for admin panel
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [file, setFile] = useState("");
+    // handler for auth
+    const [isAuth, setAuth] = useState(false);
+    // view for login
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleUpload = async () => {
         // upload the blog to the firestore
@@ -24,40 +32,86 @@ export const Admin = () => {
                 console.log(e);
             });
         });
-        
     }
 
+    const handleSignIn = () => {
+        signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            console.log("Signed in as:");
+            console.log(userCredential.user);
+            setAuth(true);
+        }).catch((e) => {
+            console.log(e);
+        })
+    }
+
+    useEffect(() => {
+        auth.onAuthStateChanged((authObj => {
+            if (authObj) {
+                setAuth(true);
+            } else {
+                setAuth(false);
+            }
+        }));
+    });
+
     return (
-        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-2 text-osuch-black">
-            <h1 className="text-5xl font-semibold">Admin Panel</h1>
-            <h2 className="text-3xl font-medium">Add Blog</h2>
-            <input
-                type="text"
-                placeholder="title"
-                onChange={(e) => {
-                    setTitle(e.target.value);
-                }}
-                className="border border-black p-2 rounded-md w-1/3"
-            />
-            <input
-                type="text"
-                placeholder="description"
-                onChange={(e) => {
-                    setDescription(e.target.value);
-                }}
-                className="border border-black p-2 rounded-md w-1/3"
-            />
-            <input
-                type="file"
-                className="w-1/3"
-                onChange={(event) => {
-                    setFile(event.target.files[0]);
-                }}
-            />
-            <button
-                className="bg-osuch-black text-white pl-8 pr-8 pt-2 pb-2 rounded-lg"
-                onClick={handleUpload}
-            >submit</button>
+        <div className="min-h-screen bg-gray-100 text-osuch-black">
+            {isAuth ?
+                <div className="h-screen flex flex-col items-center justify-center space-y-2">
+                    <h1 className="text-3xl font-medium">Add Blog</h1>
+                    <input
+                        type="text"
+                        placeholder="title"
+                        onChange={(e) => {
+                            setTitle(e.target.value);
+                        }}
+                        className="border border-black p-2 rounded-md w-1/3"
+                        value={title}
+                    />
+                    <input
+                        type="text"
+                        placeholder="description"
+                        onChange={(e) => {
+                            setDescription(e.target.value);
+                        }}
+                        className="border border-black p-2 rounded-md w-1/3"
+                        value={description}
+                    />
+                    <input
+                        type="file"
+                        className="w-1/3"
+                        onChange={(event) => {
+                            setFile(event.target.files[0]);
+                        }}
+                    />
+                    <button
+                        className="bg-osuch-black text-white pl-8 pr-8 pt-2 pb-2 rounded-lg"
+                        onClick={handleUpload}
+                    >submit</button>
+                </div> :
+                <div className="h-screen flex flex-col items-center justify-center space-y-2">
+                    <h1 className="text-3xl font-medium">Sign In</h1>
+                    <input
+                        type="email"
+                        placeholder="email"
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        className="border border-black p-2 rounded-md w-1/6"
+                    />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                        className="border border-black p-2 rounded-md w-1/6"
+                    />
+                    <button
+                        className="bg-osuch-black text-white pl-8 pr-8 pt-2 pb-2 rounded-lg"
+                        onClick={handleSignIn}
+                    >submit</button>
+                </div>}
         </div>
     );
 }
